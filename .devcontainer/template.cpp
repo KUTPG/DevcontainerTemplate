08635d43159ce5927ff38912a9597ac1d19bc82f
MineArchive <102400088+minearchive@users.noreply.github.com>
2026-04-23 01:58:56 +0900

    del: remove beginner-unfriendly code

--- .devcontainer/template.cpp
+++ .devcontainer/template.cpp
@@ -2,23 +2,10 @@
 using namespace std;
 
 using ll = long long;
-using vl = vector<ll>;
-using vvl = vector<vl>;
-using pll = pair<ll, ll>;
-
-#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
-#define rep2(i, a, b) for (ll i = (a); i < (ll)(b); i++)
-#define all(x) (x).begin(), (x).end()
-#define sz(x) ((ll)(x).size())
-
-const ll INF = 1e18;
-const ll MOD = 1e9 + 7;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-
-
-    return 0;
+	return 0;
 }
