dabd525c1fff8434da137ceaa0298b3462147fcc
minearchive <102400088+minearchive@users.noreply.github.com>
2026-04-23 10:56:00 +0900

    fix: command can't use when clone directory name is different

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# テンプレートをホームディレクトリに固定パスでコピー
+cp "$(pwd)/.devcontainer/template.cpp" ~/.cpp_template
+
 cat << 'EOF' >> ~/.bashrc
 
-TEMPLATE=/workspaces/DevcontainerTemplate/.devcontainer/template.cpp
-
 new() {
-    cp $TEMPLATE ${1}.cpp
+    cp ~/.cpp_template "${1}.cpp"
     echo "Created ${1}.cpp"
-    code ${1}.cpp
+    code "${1}.cpp"
 }
 
 gpp() {
-    g++ ${1} -o ./a.out
+    g++ "${1}" -o ./a.out
 }
 
 EOF
